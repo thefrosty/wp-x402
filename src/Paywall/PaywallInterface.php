1133bd3c72d2b1c5b67c73c743671dcca3cb929c
Austin Passy <367897+thefrosty@users.noreply.github.com>
2025-12-21 14:36:31 -0800

    Remove ASSET

--- src/Paywall/PaywallInterface.php
+++ src/Paywall/PaywallInterface.php
@@ -10,7 +10,6 @@ namespace TheFrosty\WpX402\Paywall;
  */
 interface PaywallInterface
 {
-    public const float DEFAULT_PRICE = 0.01;
-    final public const string TESTNET_ASSET = '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913'; // phpcs:ignore
+    public const float DEFAULT_PRICE = 0.001;
     final public const string TESTNET_WALLET = '0x505bc35f0a83c9ed06c6f94e68f0f86cf2812a6b'; // phpcs:ignore
 }
