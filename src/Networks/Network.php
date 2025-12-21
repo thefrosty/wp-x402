9b67bba239b2ee7e074ed30715022af79e4fea1d
Austin Passy <367897+thefrosty@users.noreply.github.com>
2025-12-21 15:02:33 -0800

    Add Network interface for Networks enums

---
+++ src/Networks/Network.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TheFrosty\WpX402\Networks;
+
+/**
+ * Interface Network
+ * @package TheFrosty\WpX402\Networks
+ */
+interface Network
+{
+
+}
--- src/Networks/Testnet.php
+++ src/Networks/Testnet.php
@@ -12,11 +12,11 @@ namespace TheFrosty\WpX402\Networks;
  * @ref https://docs.cdp.coinbase.com/get-started/supported-networks
  * @ref https://docs.cdp.coinbase.com/x402/quickstart-for-sellers#network-identifiers-caip-2
  */
-enum Testnet: string
+enum Testnet: string implements Network
 {
-    // USDC Contract Addresses: https://developers.circle.com/stablecoins/usdc-contract-addresses
-    case ASSET_BASE = '0x036CbD53842c5426634e7929541eC2318f3dCF7e';
-    case ASSET_ETHEREUM = '0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238';
+    // USDC Contract Addresses: https://developers.circle.com/stablecoins/usdc-contract-addresses.
+    case ASSET_BASE = '0x036CbD53842c5426634e7929541eC2318f3dCF7e'; // phpcs:ignore
+    case ASSET_ETHEREUM = '0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238'; // phpcs:ignore
 
     case BASE = 'eip155:84532';
     case DEVNET = 'solana:EtWTRABZaYq6iMfeYKouRu166VU2xqa1';
