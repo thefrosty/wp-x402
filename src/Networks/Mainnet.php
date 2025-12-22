1c01cbbd9b466674739819e6163031f8603a7e88
Austin Passy <367897+thefrosty@users.noreply.github.com>
2025-12-22 09:56:58 -0800

    Change to Solana

--- src/Networks/Mainnet.php
+++ src/Networks/Mainnet.php
@@ -15,7 +15,7 @@ enum Mainnet: string implements Network
 {
     // USDC Contract Addresses: https://developers.circle.com/stablecoins/usdc-contract-addresses.
     case ASSET_BASE = '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913'; // phpcs:ignore
-    case ASSET_ETHEREUM = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'; // phpcs:ignore
+    case ASSET_SOLANA = 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'; // phpcs:ignore
 
     case BASE = 'eip155:8453';
     case SOLANA = 'solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp';
--- src/Networks/Testnet.php
+++ src/Networks/Testnet.php
@@ -16,7 +16,7 @@ enum Testnet: string implements Network
 {
     // USDC Contract Addresses: https://developers.circle.com/stablecoins/usdc-contract-addresses.
     case ASSET_BASE = '0x036CbD53842c5426634e7929541eC2318f3dCF7e'; // phpcs:ignore
-    case ASSET_ETHEREUM = '0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238'; // phpcs:ignore
+    case ASSET_SOLANA = '4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU'; // phpcs:ignore
 
     case BASE = 'eip155:84532';
     case SOLANA = 'solana:EtWTRABZaYq6iMfeYKouRu166VU2xqa1';
